bf772315891884cf42f6a3cffc53a7a92025fe95
minjae_Jang <wkdanlwo091@gmail.com>
2020-09-07 13:10:07 +0900

    kakao 2019 01 added

--- algorithm/src/algorithm/Katalk_1.java
+++ algorithm/src/algorithm/Katalk_1.java
@@ -5,55 +5,64 @@ public class Katalk_1 {
 	
 	
 	public int solution(String s) {
-		int answer = 0;
+		int answer = s.length();
+		int tempAnswer = 0;//원본 문자열 길이
 		
-		int temp = 0;
-		for(int i =1 ;i< s.length()/2;i++) {// i개로 자르기 
-			temp = 0;// 잘라서 나온 길이
-			for(int j = 0 ;j < s.length(); ) {//j 기준으로 비교하기
-				int temp2 = 0;//덩어리당 더한 갯수  ---> aaa 
-				for(int k = j ; k < s.length(); k++) {// 
-					if(k+i <=s.length()) {//비교대상이 index안에 있을 때
-						if(s.substring(j, j+i).equals(s.substring(k, k+i))) {
-							temp2++;//자신부터 검사  i = 2 aaaa이면 temp는 2
-							if(k == s.length()-1) {
-								if(temp2 > 1)//aaaa면       
-									temp = temp + 1 + i;//2aa의 길이 3 
-								else if(temp2 == 1)//aa면
-									temp = temp + i;// aa의 길이는 2 
-								j = k;
-								break;
-							} 
-
-						}else {// ex) ab면 a와 b가 다른 상태
-
-							if(temp2 > 1)//aaaa면       
-								temp = temp + 1 + i;//2aa의 길이 3 
-							else if(temp2 == 1)//aa면
-								temp = temp + i;// aa의 길이는 2 
-							
-							//기준점을 b로 잡는다.
-							
-							j = k;//b부터 다시 조사
-							break;
-						}
-					}else {//index가 벗어났을 때  ex) i = 3 aa이면
-						temp = s.length()- j; //j부터 끝까지 
-					}
-				}
-				
-			}
-			if(temp < answer) {
-				System.out.println(answer);
-				answer = temp;
-			}
-		}
-			
+        for(int i = 1 ;i <= s.length()/2 ;i++){//i개로 압축하기 , 최대 압축은 문자열길이의 반이다. 
+            
+        	tempAnswer = 0;
+        	String temp = s.substring(0, i);
+        	int index = 0;
+        	
+        	for(int j = 0; j < s.length(); j = j+i) {
+        		
+        		if(j+i > s.length()) {//덩어리가 last index를 넘어갈때 last index까지 짜른다. 
+        			if(index == 1)
+        				tempAnswer = tempAnswer + i;//전에 증가한 index 더하고 
+        			else if(index != 1) {
+        				tempAnswer = tempAnswer + Integer.toString(index).length() + i;// 50a일때     숫자 길이 + 자른 숫자  2+1
+        			}
+        			tempAnswer  = tempAnswer + s.length()-j;
+        			break;
+        		}
+        		
+        		if( temp.equals(s.substring(j, j+i))) {//같다.
+        			System.out.println("1 " + j);
+        			index++;
+        		}
+        		else {
+        			System.out.println("2 " + j);
+        			if(index == 1)
+        				tempAnswer = tempAnswer + i;//전에 증가한 index 더하고 
+        			else if(index != 1) {
+        				tempAnswer = tempAnswer + Integer.toString(index).length() + i;// 50a일때     숫자 길이 + 자른 숫자  2+1
+        			}
+        			System.out.println("tempAnswer는" + tempAnswer);
+        			index = 1;//인덱스 한개로 복귀
+        			temp = s.substring(j, j+i);
+        		}
+        		
+        		if(j == s.length()-i) {//마지막 index 
+        			System.out.println("3" + j);
+        			if(index == 1) {
+        				tempAnswer = tempAnswer +i;
+        			}else if(index != 1) {
+        				tempAnswer = tempAnswer + Integer.toString(index).length() + i;// 50a일때     숫자 길이 + 자른 숫자  2+1
+        			}
+        		}
+        		
+        	}
+        	if(tempAnswer < answer ) {
+        		answer = tempAnswer;
+        	}
+        	
+        	
+        }
 	    return answer;
 	}
 
 	public static void main(String[] args) {
 		Katalk_1 a  =new Katalk_1();
-		System.out.println(a.solution("aabbaccc"));
+		System.out.println(a.solution("xababcdcdababcdcd"));
 	}
 }
